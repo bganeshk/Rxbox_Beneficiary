78695918767dc7ad4bc236119f95ccb7dfaf2699
ganesba1@in.ibm.com <ganesba1@in.ibm.com>
2020-05-14 18:42:11 +0530

    first change

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/cl-Rxbox'),
+      dir: require('path').join(__dirname, './coverage/rxbox-beneficiary'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
