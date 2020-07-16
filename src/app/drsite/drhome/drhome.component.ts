b461a6050ae67872e38cab2f90d1a001f8b5b2f1
ganesba1@in.ibm.com <ganesba1@in.ibm.com>
2020-07-16 16:45:42 +0530

    added review record

--- src/app/drsite/drhome/drhome.component.ts
+++ src/app/drsite/drhome/drhome.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
 import { DrViewdataService, Review} from '../../global/dr-viewdata.service';
-import { MessageService } from 'primeng/api';
+import { MessageService, SelectItem } from 'primeng/api';
 import { constructor } from 'echarts';
 
 @Component({
@@ -10,10 +10,12 @@ import { constructor } from 'echarts';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DrhomeComponent implements OnInit {
-
+  typeLst:SelectItem[]
   reviewDetails:Review[]
+  newRecType:string;
   selectedReviewDetails:Review;
   @Output() onReviewSelect= new EventEmitter<string>();
+  viewDlg: boolean;
 
   constructor(private drViewSrvs: DrViewdataService, private messageService: MessageService) {
     
@@ -21,18 +23,32 @@ export class DrhomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.reviewDetails=this.drViewSrvs.getReviewHistory();
+    this.typeLst=[
+      {label:'Review Appointment',value:'review_rec'},
+      {label:'Hi-Low Entry',value:'hilow_rec'},
+      {label:'Family Details',value:'family_rec'},
+      {label:'Lifestyle Entry',value:'lifesty_rec'},
+      {label:'Vaccinations',value:'vacnan'},      
+      {label:'Summary Record',value:'summary_rec'},
+      {label:'Other Docs',value:'odoc_rec'},
+      
+    ]
   }
 
   onReviewRowSelect(){
     this.onReviewSelect.emit(this.selectedReviewDetails.refNo);
   }
   addNewReviewData(){
-
+    this.viewDlg=true;
   }
   onDelete(dm:Review){
 
   }
   onEdit(dm:Review){
     
+  }
+  saveNewRview(){
+    this.viewDlg=false;
+    this.messageService.add({ sticky: false, severity: 'success', summary: 'Record Saved', detail: 'Record has been saved successfully' });
   }
 }
